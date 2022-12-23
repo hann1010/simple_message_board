1eef743652064ec611121d6ad770a37a4efdf851
hann1010 <hanna.paivasto@gmail.com>
2022-12-23 22:32:26 +0200

    users set up

--- simple_message_board/settings.py
+++ simple_message_board/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
+    'users.apps.UsersConfig',  # users
 ]
 
 MIDDLEWARE = [
