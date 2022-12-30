389519f402b7693573a034a5168780716ece79d0
hann1010 <hanna.paivasto@gmail.com>
2022-12-30 18:24:03 +0200

    Fix forum

--- forum/urls.py
+++ forum/urls.py
@@ -13,17 +13,17 @@ from .views import (
 
 
 urlpatterns = [
-    path('', views.home, name='forum-home'),
-    path('latest/topics/', views.latest_topics, name='forum-latest_topics'),
-    path('latest/comments/', views.latest_comments, name='forum-latest_comments'),
-    path('latest/all/', views.latest_all, name='forum-latest_all'),
-    path('topic/new/', TopicCreateView.as_view(), name='forum_topic-create'),
-    path('all/<int:pk>/comment/new/', CommentCreateView.as_view(), name='forum_comment-create'),
-    path('all/<int:pk>/open/', AllDetailView.as_view(), name='forum_open_one_post'),
-    path('all/<int:pk>/thread/', ThreadDetailView.as_view(), name='forum_thread'),
-    path('all/<int:pk>/update/', PostUpdateView.as_view(), name='forum_all_update'),
-    path('all/<int:pk>/delete/', PostDeleteView.as_view(), name='forum_all_delete'),
-    path('all/<int:pk>/user_info/', UserDetailView.as_view(), name='forum_user_info'),
+    path('', views.home, name='message-board_home'),
+    path('forum/latest/topics/', views.latest_topics, name='forum-latest_topics'),
+    path('forum/latest/comments/', views.latest_comments, name='forum-latest_comments'),
+    path('forum/latest/all/', views.latest_all, name='forum-latest_all'),
+    path('forum/topic/new/', TopicCreateView.as_view(), name='forum_topic-create'),
+    path('forum/all/<int:pk>/comment/new/', CommentCreateView.as_view(), name='forum_comment-create'),
+    path('forum/all/<int:pk>/open/', AllDetailView.as_view(), name='forum_open_one_post'),
+    path('forum/all/<int:pk>/thread/', ThreadDetailView.as_view(), name='forum_thread'),
+    path('forum/all/<int:pk>/update/', PostUpdateView.as_view(), name='forum_all_update'),
+    path('forum/all/<int:pk>/delete/', PostDeleteView.as_view(), name='forum_all_delete'),
+    path('forum/all/<int:pk>/user_info/', UserDetailView.as_view(), name='forum_user_info'),
     
 	
 ]
