5855285b2d4eddaefc369ad8087dc19f2db76448
hann1010 <hanna.paivasto@gmail.com>
2023-01-16 19:33:18 +0200

    chat fix

--- chat/views.py
+++ chat/views.py
@@ -80,7 +80,7 @@ class Chat_View(LoginRequiredMixin, CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Chat view'
-        context['title_page'] = 'Chat'
+        context['title_page'] = 'Simple message board'
         return context
 
     def get_template_names(self):
