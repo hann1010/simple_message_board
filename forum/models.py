a741829676e3b1973e5e785af0df6d05a1a15075
hann1010 <hanna.paivasto@gmail.com>
2023-01-17 20:25:17 +0200

    forum fix

--- forum/models.py
+++ forum/models.py
@@ -10,6 +10,8 @@ class Forum_post(models.Model):
     content = RichTextField()
     date_posted = models.DateTimeField(default=timezone.now)
     date_last_save = models.DateTimeField(auto_now=timezone.now)
+    author_name = models.CharField(max_length=100, blank=True)
+    author_nickname = models.CharField(max_length=100, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
