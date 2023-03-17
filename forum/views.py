e5c64b0deae84738f79af4772e0e89bc49abbe50
hann1010 <hanna.paivasto@gmail.com>
2023-03-17 20:15:01 +0200

    forum_fix

--- forum/views.py
+++ forum/views.py
@@ -154,6 +154,7 @@ class AllDetailView(LoginRequiredMixin, DetailView): #Show one post
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = 'Forum one post'
+        context["title"] = 'Simple message board'
         return context
 
 
