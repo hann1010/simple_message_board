fe72d835e9790614669a0b6c1075c9801e9e7037
hann1010 <hanna.paivasto@gmail.com>
2023-03-28 21:39:15 +0300

    forum_fix

--- forum/views.py
+++ forum/views.py
@@ -192,7 +192,7 @@ class UserDetailView(LoginRequiredMixin, DetailView): #Show selected user inform
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = 'User info'
+        context['title'] = 'User info'
         return context
 
 
