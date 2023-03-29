693f01f99a25d06d4ceafe0b97335635136657eb
hann1010 <hanna.paivasto@gmail.com>
2023-03-29 21:04:55 +0300

    forum_fix

--- forum/views.py
+++ forum/views.py
@@ -202,7 +202,7 @@ class UserHomeDetailView(LoginRequiredMixin, DetailView): #Show selected home us
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = 'User info'
+        context['title'] = 'User info'
         return context
 
 
